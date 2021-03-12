9c015bff7fe2c02127346076a1aeea2bc1724a11
Marco Zamana <marco.zamana1@gmail.com>
2021-03-12 10:54:16 +0100

    update

--- Scalable.WebAPI/Program.cs
+++ Scalable.WebAPI/Program.cs
@@ -8,6 +8,7 @@ namespace Scalable.WebAPI
         public static void Main(string[] args)
         {
             CreateWebHostBuilder(args).Build().Run();
+
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
